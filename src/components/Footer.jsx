61c106dd198b7a67e97e7ce25b5c305e68c1f8d1
Akpokighe Marvellous O <m.ovigwe@yahoo.com>
2022-11-05 11:22:20 +0100

    Contents - Added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,13 +10,9 @@ const Footer = () => {
       <div className="mb-8 w-full text-left md:flex md:space-x-64 md:justify-center md:align-middle">
         <img src={Zuri} alt="zuri" className="w-30 border-sky-500 mb-2" />
         <span className="text-gray-500 mb-2">
-         
+          HNG Internship 9 Frontend Task
         </span>
-        <img
-          src={Ingresive}
-          alt="IG4"
-          className="w-28 border-sky-500 mt-2 md:w-28"
-        />
+      
       </div>
     </footer>
   );
